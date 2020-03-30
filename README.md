943f1d3d5c081fb7b703e720661417b952714e6e
Samir millan orozco <samirmillanorozco@hotmail.com>
2020-03-30 17:33:44 +0000

    Update create

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 #"Samir millan orozco"  <samirmillanorozco@hotmail.com> - https://twitter.com/Gaspela
 
 #"Diego Felipe Quijano Zuñiga"- <dfqz93@hotmail.com> - https://twitter.com/DiegoQ0096
+    
--- console.py
+++ console.py
@@ -43,7 +43,22 @@ class HBNBCommand(cmd.Cmd):
                 raise SyntaxError()
             my_list = line.split(" ")
             obj = eval("{}()".format(my_list[0]))
+            """ Update console def do_create """
+            arg_list = my_list[1:]
+            for i in arg_list:
+                if "=" in i:
+                    arg_split = i.split("=")
+                    if arg_split[1][0] == "\"":
+                        arg_split[1] = arg_split[1][1:-1]
+                        arg_split[1] = arg_split[1].replace(
+                            '_', ' ').replace('"', '\\"')
+                    elif arg_split[1].isdigit():
+                        arg_split[1] = int(arg_split[1])
+                    else:
+                        arg_split[1] = float(arg_split[1])
+                    setattr(obj, arg_split[0], arg_split[1])
             obj.save()
+            """ Update console def do_create """
             print("{}".format(obj.id))
         except SyntaxError:
             print("** class name missing **")
