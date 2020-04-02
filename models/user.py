b854b603a304f87d76ea865e0a1329d1e8ac3f4d
diego0096 <dfqz93@hotmail.com>
2020-04-02 00:41:14 +0000

    User commit

--- models/user.py
+++ models/user.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class User(BaseModel):
@@ -11,7 +15,10 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
