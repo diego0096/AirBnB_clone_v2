970cef48c80b11a7f51d48f6e36f089895530440
diego0096 <dfqz93@hotmail.com>
2020-04-02 17:13:06 +0000

    fix test, dev

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
 -- Data base test
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
-SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db` . * TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema` . * TO 'hbnb_test'@'localhost';
+DROP USER IF EXISTS hbnb_test@localhost;
 FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
