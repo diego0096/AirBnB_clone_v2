de5ba71168ccc8def2988f372687b06de2501356
Samir millan orozco <Samirmillanorozco@hotmail.com>
2020-04-02 21:12:18 +0000

    Comments .sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,9 @@
--- Data base dev
+-- Create a database
+-- Create hbnb_dev_db if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-DROP USER IF EXISTS hbnb_dev@localhost;
-FLUSH PRIVILEGES;
-CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Create user:hbnb_dev with password 'hbnb_dev_pwd'
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Privilege to hbnb_dev for database: hbnb_dev_db
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Privilege to hbnb_dev on database: performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,9 @@
--- Data base test
+-- Create a database
+-- Create hbnb_test_db if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-DROP USER IF EXISTS hbnb_test@localhost;
-FLUSH PRIVILEGES;
-CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- Create user:hbnb_test with password 'hbnb_test_pwd'
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- Privilege to hbnb_test on database: hbnb_test_db
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- Privilege to hbnb_test on database: performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
