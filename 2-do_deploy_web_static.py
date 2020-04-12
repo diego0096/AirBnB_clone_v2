81ec39f027bdd45037ed97a92ac0fdc439722ca1
diego0096 <dfqz93@hotmail.com>
2020-04-12 04:09:44 +0000

    second commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,26 +1,38 @@
 #!/usr/bin/python3
-'''deploy web_static to server'''
-
+"""compress a folder """
+from datetime import datetime
 from fabric.api import *
+import shlex
 import os
 
-env.user = 'ubuntu'
-env.hosts = ['35.196.233.196', '35.237.182.135']
-
+env.hosts = ['35.231.33.237', '34.74.155.163']
+env.user = "ubuntu"
 
 def do_deploy(archive_path):
-    """Fabric script to deploy web_static to servers"""
-    if os.path.exists(archive_path):
-        new_path = archive_path[9:]
-        de_path = '/data/web_static/releases/{}/'.format(new_path)[0:-5]
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(de_path))
-        run('tar -xzf /tmp/{} -C {}'.format(new_path, de_path))
-        run('rm /tmp/{}'.format(new_path))
-        run('mv  {}/web_static/* {}'.format(de_path, de_path))
-        run('rm -rf {}/web_static'.format(de_path))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(de_path))
-        print('New version deployed successfully!')
+    """ Deploys """
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
         return True
-    return False
+    except:
+        return False
